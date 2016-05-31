cce9857c53f3c4306e99965c417307c4cafb8e5b
litao008 <litao@binhong-tech.com>
2016-05-31 15:50:59 +0800

    litao 1

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -13,6 +13,7 @@ namespace HelloWorld
         [STAThread]
         static void Main()
         {
+//litao 1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
